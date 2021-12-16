601b91e05426dcdc0304084e307acfd41e5aaa53
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2021-12-16 10:29:12 +0100

    feat(server): add auth system

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { BikesModule } from './bikes/bikes.module';
 import { ParksModule } from './parks/parks.module';
 import { StationsModule } from './stations/stations.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BikesModule, ParksModule, StationsModule, UsersModule],
+  imports: [AuthModule, BikesModule, ParksModule, StationsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })

01fecaf595a968f84d3d745aaa5274d8d3bd4250
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2021-12-16 09:18:51 +0100

    feat(repositories): add repositories lib

---
+++ libs/repositories/src/lib/bike.ts
@@ -0,0 +1,71 @@
+import { BikeDTO } from '@gui-nx/types';
+import axios from 'axios';
+import { API_BASE_URL } from './constants';
+import { getError } from './utilities';
+
+export class BikeRepository {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
+  private static _instance?: BikeRepository;
+
+  static get(): BikeRepository {
+    if (!this._instance) {
+      this._instance = new BikeRepository();
+    }
+
+    return this._instance;
+  }
+
+  async getBikes(): Promise<BikeDTO[]> {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/bikes`);
+
+      return response.data.bikes;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+
+  async getBike(id: string): Promise<BikeDTO | undefined> {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/bikes/${id}`);
+
+      return response.data.bike;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+
+  async createBike(bike: Omit<BikeDTO, 'id'>): Promise<BikeDTO> {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/bikes`, bike);
+
+      return response.data.bike;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+
+  async editBike(
+    id: string,
+    bike: Partial<Omit<BikeDTO, 'id'>>
+  ): Promise<BikeDTO | undefined> {
+    try {
+      const response = await axios.patch(`${API_BASE_URL}/bikes/${id}`, bike);
+
+      return response.data.bike;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+
+  async deleteBike(id: string): Promise<boolean> {
+    try {
+      await axios.delete(`${API_BASE_URL}/bikes/${id}`);
+
+      return true;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+}
---
+++ libs/repositories/src/lib/station.ts
@@ -0,0 +1,74 @@
+import { StationDTO } from '@gui-nx/types';
+import axios from 'axios';
+import { API_BASE_URL } from './constants';
+import { getError } from './utilities';
+
+export class StationRepository {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
+  private static _instance?: StationRepository;
+
+  static get(): StationRepository {
+    if (!this._instance) {
+      this._instance = new StationRepository();
+    }
+
+    return this._instance;
+  }
+
+  async getStations(): Promise<StationDTO[]> {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/stations`);
+
+      return response.data.stations;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+
+  async getStation(id: string): Promise<StationDTO | undefined> {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/stations/${id}`);
+
+      return response.data.station;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+
+  async createStation(station: Omit<StationDTO, 'id'>): Promise<StationDTO> {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/stations`, station);
+
+      return response.data.station;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+
+  async editStation(
+    id: string,
+    station: Partial<Omit<StationDTO, 'id'>>
+  ): Promise<StationDTO | undefined> {
+    try {
+      const response = await axios.patch(
+        `${API_BASE_URL}/stations/${id}`,
+        station
+      );
+
+      return response.data.station;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+
+  async deleteStation(id: string): Promise<boolean> {
+    try {
+      await axios.delete(`${API_BASE_URL}/stations/${id}`);
+
+      return true;
+    } catch (e) {
+      throw getError(e);
+    }
+  }
+}
